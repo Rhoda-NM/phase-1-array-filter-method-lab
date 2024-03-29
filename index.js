31254dbed7a505617019b4a3c92a6ceb5fa61cf3
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-03-29 13:15:40 +0300

    complete filter lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(driversArray, passedString) {
+    const matchingDrivers = driversArray.filter((driver) => driver.toUpperCase() == passedString.toUpperCase());
+    return matchingDrivers;
+}
+function fuzzyMatch(driversArray, passedString) {
+    const matchingLetter = driversArray.filter((driver) => driver[0] === passedString[0]);
+    return matchingLetter;
+}
+function matchName(driversArray, passedString) {
+    const matchingName = driversArray.filter((driver) => driver.name === passedString);
+    return matchingName
+}
